de55ebc2d4d4c37006beaadb1df590ee59566262
GeraJuarez <geracjuarez@gmail.com>
2019-06-20 23:13:43 -0500

    add db instance

---
+++ src/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const dev_db_url = config.db_url;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
