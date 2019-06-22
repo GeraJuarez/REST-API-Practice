463b0e050ce787d2ec0e75d2bac1be8bf27519f1
GeraJuarez <geracjuarez@gmail.com>
2019-06-22 10:08:53 -0500

    remove test models

--- src/models/userA.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');  
-const Schema = mongoose.Schema;
-
-const UserA = new Schema({
-    class: String
-});
-
-module.exports = mongoose.model('UserA', UserA);
--- src/models/userB.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');  
-const Schema = mongoose.Schema;
-
-const UserB = new Schema({
-    school: String
-});
-
-module.exports = mongoose.model('UserB', UserB);
