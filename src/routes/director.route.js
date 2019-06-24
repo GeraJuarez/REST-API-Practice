528e1b7d7e7d212554b30d47fa79f5fc66bd8dc6
GeraJuarez <geracjuarez@gmail.com>
2019-06-24 13:46:10 -0500

    add token validation as test

--- src/routes/director.route.js
+++ src/routes/director.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const verifyToken = require('../middleware/VerifyToken');
 
 const directorController = require('../controllers/director.controller');
 
 router.get('/', directorController.directorGetAll);
 router.get('/:id', directorController.directorGetById);
-router.post('/', directorController.directorCreate);
+router.post('/', verifyToken.isDirector, directorController.directorCreate);
 router.put('/:id', directorController.directorUpdate);
 router.delete('/:id', directorController.directorDelete);
 
